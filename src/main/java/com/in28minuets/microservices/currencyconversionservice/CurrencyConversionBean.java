ff603cb9b0576df24f44993a93d0c21c677fcd60
Aravind <aravindreddy.vbit@gmail.com>
2019-01-19 15:43:42 -0800

    new coommit

---
+++ src/main/java/com/in28minuets/microservices/currencyconversionservice/CurrencyConversionBean.java
@@ -0,0 +1,74 @@
+package com.in28minuets.microservices.currencyconversionservice;
+
+import java.math.BigDecimal;
+
+public class CurrencyConversionBean {
+
+	private Long id;
+	private String from;
+	private String to;
+	private BigDecimal conversionFactor;
+	private BigDecimal quantity;
+	public BigDecimal getTotalCalculatedAmount() {
+		return totalCalculatedAmount;
+	}
+	public void setTotalCalculatedAmount(BigDecimal totalCalculatedAmount) {
+		this.totalCalculatedAmount = totalCalculatedAmount;
+	}
+	private BigDecimal totalCalculatedAmount;
+	private int port;
+	
+	
+	public CurrencyConversionBean(Long id, String from, String to, BigDecimal conversionFactor, BigDecimal quantity,
+			BigDecimal totalCalculatedAmount, int port) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionFactor = conversionFactor;
+		this.quantity = quantity;
+		this.totalCalculatedAmount = totalCalculatedAmount;
+		this.port = port;
+	}
+	public CurrencyConversionBean() {
+		// TODO Auto-generated constructor stub
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	public String getTo() {
+		return to;
+	}
+	public void setTo(String to) {
+		this.to = to;
+	}
+	public BigDecimal getConversionFactor() {
+		return conversionFactor;
+	}
+	public void setConversionFactor(BigDecimal conversionFactor) {
+		this.conversionFactor = conversionFactor;
+	}
+	public BigDecimal getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(BigDecimal quantity) {
+		this.quantity = quantity;
+	}
+	public int getPort() {
+		return port;
+	}
+	public void setPort(int port) {
+		this.port = port;
+	}
+	
+	
+}
